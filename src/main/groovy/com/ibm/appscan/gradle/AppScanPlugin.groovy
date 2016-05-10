35ddfa983cbb12f49c081f67e363790a67c5317c
Matt Murphy <mattmurp@us.ibm.com>
2016-05-09 20:58:11 -0400

    add task descriptions

--- src/main/groovy/com/ibm/appscan/gradle/AppScanPlugin.groovy
+++ src/main/groovy/com/ibm/appscan/gradle/AppScanPlugin.groovy
@@ -10,7 +10,7 @@ class AppScanPlugin implements Plugin<Project> {
 			
 		project.extensions.create("appscansettings", AppScanSettingsExtension, project)
 	
-        	project.task('createProject') << {
+        	project.task('createProject', description: "Generates an AppScan Source project for this Gradle project.") << {
 			try {
 				new ProjectCreator(project).run();
 			} catch (AppScanException e) {
@@ -18,15 +18,15 @@ class AppScanPlugin implements Plugin<Project> {
 			}
 		}
 		
-		project.task('createCLIScript') << {
+		project.task('createCLIScript', description: "Generates an AppScan Source CLI script for executing a scan.") << {
 			try {
 				new ScriptCreator(project).run();
 			} catch (AppScanException e) {
 				throw new TaskExecutionException(project.createCLIScript, e);
 			}
 		}
-		
-		project.task('runScan') << {
+	
+		project.task('runScan', description: "Executes the AppScan Source CLI script located at <scriptdir>/cliscript.txt.") << {
 			try {
 				new ScanRunner(project).run();
 			} catch (AppScanException e) {
